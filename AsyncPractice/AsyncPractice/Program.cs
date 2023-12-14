4d5acb8a99c847253c85904ae65a12105f1e8597
Lizzy227 <lisa@sandwichtrading.com>
2023-12-14 09:43:08 +0200

    added tasks

---
+++ AsyncPractice/AsyncPractice/Program.cs
@@ -0,0 +1,19 @@
+﻿//See https://aka.ms/new-console-template for more information
+
+
+
+static async Task Main(string[] args)
+{
+    Console.WriteLine("Starting...");
+
+    await DelayedPrint("Async operation completed.", 2000);
+
+    Console.WriteLine("All done!");
+}
+static async Task DelayedPrint(string message, int millisecondsDelay)
+{
+    await Task.Delay(millisecondsDelay);
+    Console.WriteLine(message);
+}
+
+
