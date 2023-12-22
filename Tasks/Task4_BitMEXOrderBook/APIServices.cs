f1e48d14c104852b75ebb22f01918b3a2bf81b98
Lizzy227 <lisa@sandwichtrading.com>
2023-12-22 13:51:09 +0200

    Adding to APIServices class
    -Changed OrderBook.cs to Orderbook.cs
    -Added method to initialize HTTPClient

---
+++ Tasks/Task4_BitMEXOrderBook/APIServices.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+
+namespace Task4_BitMEXOrderbook
+{
+    internal class APIServices
+    {
+        private readonly HttpClient client;
+
+        public APIServices()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://testnet.bitmex.com/api/v1"); 
+            InitializeHttpClient();
+        }
+
+        public void InitializeHttpClient()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<Orderbook> GetOrderBookAsync(string symbol)
+        {
+            try
+            {
+                using (HttpResponseMessage response = await client.GetAsync($"/orderbook/L2?symbol={symbol}&depth=25"))
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Orderbook orderBook = JsonConvert.DeserializeObject<Orderbook>(responseBody);
+                    return orderBook;
+                }
+                else
+                {
+                    // Handle unsuccessful response (e.g., log or throw exception)
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (e.g., log or throw)
+                return null;
+            }
+        }
+    }
+}
---
+++ Tasks/Task4_BitMEXOrderBook/OrderbookEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_BitMEXOrderbook
+{
+    internal class OrderbookEntry
+    {
+        public string Symbol { get; set; }
+        public int Id { get; set; }
+        public string Side { get; set; }
+        public int Size { get; set; }
+        public decimal Price { get; set; }       
+        public DateTime Timestamp { get; set; }
+
+    }
+}
