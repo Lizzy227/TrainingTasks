601a9942d68f19128344a21284d48199c1fa2742
Lizzy227 <lisa@sandwichtrading.com>
2024-02-16 07:28:00 +0200

    Fixed script, added a separate method to populate binding list

--- DGVTask/frmDGV.cs
+++ DGVTask/frmDGV.cs
@@ -10,7 +10,7 @@ namespace DGVTask
         private BindingList<Buffer> bufferList = new BindingList<Buffer>();
         public frmDGV()
         {
-            InitializeComponent();                        
+            InitializeComponent();
             LoadDataFile();
             dgvBuffers.DataSource = bufferList;
         }
@@ -26,95 +26,45 @@ namespace DGVTask
                 string[] lines = File.ReadAllLines(filepath);
                 foreach (string line in lines)
                 {
-                    string[] buffers = line.Split(',');
+                    List<string> buffers = line.Split(',').ToList();
                     foreach (string buffer in buffers)
                     {
                         if (buffer.Contains(":\\"))
                         {
-                            List<string> specialBuffers = new List<string>();
-                        StringBuilder sb = new StringBuilder();
-                        for (int i = 0; i < buffer.Length; i++)
-                        {
-                            if (buffer[i] == ':' && i + 1 < buffer.Length && buffer[i + 1] == '\\')
+                            List<string> buffersWithColonInName = new List<string>();
+                            StringBuilder sb = new StringBuilder();
+                            for (int i = 0; i < buffer.Length; i++)
                             {
-                                sb.Append(":\\");
-                                i++;
-                            }
-                            else if (buffer[i] == ':')
-                            {
-                                    specialBuffers.Add(sb.ToString());
+                                if (buffer[i] == ':' && i + 1 < buffer.Length && buffer[i + 1] == '\\')
+                                {
+                                    sb.Append(":\\");
+                                    i++;
+                                }
+                                else if (buffer[i] == ':')
+                                {
+                                    buffersWithColonInName.Add(sb.ToString());
                                     sb.Clear();
-                            }
-                                else 
+                                }
+                                else
                                 {
                                     sb.Append(buffer[i]);
                                 }
-                        }
-                       specialBuffers.Add(sb.ToString());
-                            if (specialBuffers.Count == 5)
+                            }
+                            buffersWithColonInName.Add(sb.ToString());
+                            if (buffersWithColonInName.Count == 5)
                             {
-                                string name = specialBuffers[0];
-                                string status = specialBuffers[1];
-                                string avgRuntimeString = specialBuffers[3].TrimEnd('m', 's', ' ');
-                                string throughPutString = specialBuffers[4].TrimEnd('/', 'm', 'i', 'n');
-                                int length;
-                                int avgRuntime;
-                                int throughPut;
-
-                                if (!int.TryParse(specialBuffers[2], out length))
-                                {
-                                    return;
-                                }
-
-                                if (!int.TryParse(avgRuntimeString, out avgRuntime))
-                                {
-                                    return;
-                                }
-
-                                if (!int.TryParse(throughPutString, out throughPut))
-                                {
-                                    return;
-                                }
-
-                                bufferList.Add(new Buffer { Name = name, Status = status, Length = length, AvgRuntime = avgRuntime, Throughput = throughPut });
+                                PopulateBindingList(buffersWithColonInName);
                             }
                         }
-                        else 
+                        else
                         {
-                            string[] bufferProperties = buffer.Split(':');
-                            if (bufferProperties.Length == 5)
+                            List<string> bufferProperties = buffer.Split(':').ToList();
+                            if (bufferProperties.Count == 5)
                             {
-                                string name = bufferProperties[0].Trim();
-                                string status = bufferProperties[1].Trim();
-                                string avgRuntimeString = bufferProperties[3].TrimEnd('m', 's', ' ');
-                                string throughPutString = bufferProperties[4].TrimEnd('/', 'm', 'i', 'n');
-                                int length;
-                                int avgRuntime;
-                                int throughPut;
-
-                                if (!int.TryParse(bufferProperties[2], out length))
-                                {
-                                    return;
-                                }
-
-                                if (!int.TryParse(avgRuntimeString, out avgRuntime))
-                                {
-                                    return;
-                                }
-
-                                if (!int.TryParse(throughPutString, out throughPut))
-                                {
-                                    return;
-                                }
-
-                                bufferList.Add(new Buffer { Name = name, Status = status, Length = length, AvgRuntime = avgRuntime, Throughput = throughPut });
-
-
+                                PopulateBindingList(bufferProperties);
                             }
                         }
 
-
-                        
                     }
 
                 }
@@ -122,8 +72,39 @@ namespace DGVTask
             catch (Exception)
             {
                 MessageBox.Show("Error: Something went wrong.");
-               
+
             }
+        }
+
+        private void PopulateBindingList(List<string> bufferPieces)
+        {
+            string name = bufferPieces[0].Trim();
+            string status = bufferPieces[1].Trim();
+            string avgRuntimeString = bufferPieces[3].TrimEnd('m', 's', ' ');
+            string throughPutString = bufferPieces[4].TrimEnd('/', 'm', 'i', 'n');
+            int length;
+            int avgRuntime;
+            int throughPut;
+
+            if (!int.TryParse(bufferPieces[2], out length))
+            {
+                MessageBox.Show("Error: Couldn't parse length");
+                return;
+            }
+
+            if (!int.TryParse(avgRuntimeString, out avgRuntime))
+            {
+                MessageBox.Show("Error: Couldn't parse average runtime");
+                return;
+            }
+
+            if (!int.TryParse(throughPutString, out throughPut))
+            {
+                MessageBox.Show("Error: Couldn't parse throughput");
+                return;
+            }
+
+            bufferList.Add(new Buffer { Name = name, Status = status, Length = length, AvgRuntime = avgRuntime, Throughput = throughPut });
         }
     }
 }
